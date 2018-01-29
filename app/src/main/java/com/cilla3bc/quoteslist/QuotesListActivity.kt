eaa718d0a73ae4c8cdca60564751dc5169bed317
soumya siricilla <soumya.rexel@gmail.com>
2018-01-28 16:13:43 -0800

    setup the resources and layout files

---
+++ app/src/main/java/com/cilla3bc/quoteslist/QuotesListActivity.kt
@@ -0,0 +1,14 @@
+package com.cilla3bc.quoteslist
+
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class QuotesListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes_list)
+    }
+
+}
---
+++ app/src/main/java/com/cilla3bc/quoteslist/SplashScreenActivity.kt
@@ -0,0 +1,48 @@
+package com.cilla3bc.quoteslist
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val title = findViewById<View>(R.id.titleTv) as TextView
+
+        val myTypeface = Typeface.createFromAsset(
+                this.assets,
+                "font/BLKCHCRY.TTF")
+
+        title.typeface = myTypeface
+
+        val splashThread = object : Thread() {
+
+            override fun run() {
+                try {
+                    Thread.sleep(2000)
+                    val intent = Intent(baseContext, QuotesListActivity::class.java)
+                    startActivity(intent)
+                    finish()
+
+                } catch (e: Exception) {
+
+                }
+
+            }
+        }
+        splashThread.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
+
+
